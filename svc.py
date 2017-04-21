025ff337cf37b8aeb56c46b9b28f3d46afc6b5fd
atlas604 <blitzbeat@gmail.com>
2017-04-21 05:04:36 -0400

    Add files via upload

---
+++ svc.py
@@ -0,0 +1,89 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import cv2
+from lesson_functions import *
+from data import *
+
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Define feature parameters
+color_space = 'YCrCb'         # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 11
+pix_per_cell = 8
+cell_per_block = 2
+hog_channel = 'ALL'         # Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32)     # Spatial binning dimensions
+hist_bins = 32              # Number of histogram bins
+spatial_feat = True         # Spatial features on or off
+hist_feat = True            # Histogram features on or off
+hog_feat = True             # HOG features on or off
+
+t=time.time()
+n_samples = 1000
+random_idxs = np.random.randint(0,len(cars), n_samples)
+test_cars = cars # np.array(cars)[random_idxs]
+test_notcars = notcars # np.array(notcars)[random_idxs]
+
+car_features = extract_features(test_cars, color_space=color_space, spatial_size=spatial_size,
+                        hist_bins=hist_bins, orient=orient,
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=hog_channel,
+                        spatial_feat=spatial_feat, hist_feat=hist_feat, hog_feat=hog_feat)
+
+notcar_features = extract_features(test_notcars, color_space=color_space, spatial_size=spatial_size,
+                        hist_bins=hist_bins, orient=orient,
+                        pix_per_cell=pix_per_cell, cell_per_block=cell_per_block, hog_channel=hog_channel,
+                        spatial_feat=spatial_feat, hist_feat=hist_feat, hog_feat=hog_feat)
+
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to extract HOG features...')
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:',orient,'orientations,',pix_per_cell,
+    'pixels per cell,', cell_per_block,'cells per block,',
+     hist_bins,'histogram bins, and', spatial_size,'spatial sampling.')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+
+svc_dict = { 'svc':svc,
+             'scaler':X_scaler,
+             'orient':orient,
+             'pix_per_cell':pix_per_cell,
+             'cell_per_block':cell_per_block,
+             'spatial_size':spatial_size,
+             'hist_bins':hist_bins }
+pickle.dump(svc_dict, open("svc_pickle.p", "wb" ) )
